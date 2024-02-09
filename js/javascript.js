5cb2ed2d7cec70d5712bcc411dc89599459542e9
Thajeepan-Rathiharan <thajee2002@gmail.com>
2024-02-09 13:25:54 +0000

    Updated a comment

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 // TO DO - make functions for: lightbox for images, images changing upon arrow buttons being clicked on, images rotating back to the first image, image number counter, burger menu on mobile devices, add image of product to cart with quantity counter, checkout button, text when product is added to cart, delete product icon, click on cart icon to open users cart and hide it by clicking on the cart icon again. // 
 
-// Variables to make the navigation menu on the mobile device have the ability to close and there will be slideshow thumbnails available on the desktop devices by an overlay. //
+// Here is a variable to make the navigation menu on the mobile device have the ability to close and there will be slideshow thumbnails available on the desktop devices by an overlay. //
 // Also buttons have been added to the pop up images with arrows that users can click on to go through the set of images. //
 let thumbImagesDivs = Array.from(
     document.querySelectorAll(".product-images-wrapper .thumb-image")
@@ -19,3 +19,5 @@ let thumbImagesDivs = Array.from(
   let lightBoxWrapper = document.querySelector(".lightbox-wrapper");
   let lightBoxContent = lightBoxWrapper.querySelector(".lightbox-content");
   let currentIndex = 0;
+
+  
